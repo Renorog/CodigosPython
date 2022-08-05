221cbdf988e21453961e3dd63ef941c2f3a48d9f
Renor <renornew@gmail.com>
2022-08-05 20:52:59 -0300

    corrigindo pasta

--- Licao-05-LacosDeRepeticaoparte1/numeros_impares.py
+++
@@ -1,4 +0,0 @@
-''' Faça um programa que exiba todos os valores ímpares entre 50 e 100 utilizando o range. '''
-
-for impares in range(51, 100, 2):
-    print(impares)
--- Licao-05-LacosDeRepeticaoparte1/tabuada_multiplicacao.py
+++
@@ -1,4 +0,0 @@
-for operador in range(1, 11):
-    for operador_2 in range(1, 11):
-        resultado = operador * operador_2
-        print(f"{operador} X {operador_2} = {resultado} \n")
