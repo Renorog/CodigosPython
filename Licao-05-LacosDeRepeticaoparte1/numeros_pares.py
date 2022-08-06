6fb9a7607e1ad47bac27b4b8b104c1b062cdd825
Renor <renornew@gmail.com>
2022-08-05 21:01:45 -0300

    corrigindo .gitignore

---
+++ Licao-05-LacosDeRepeticaoparte1/numeros_pares.py
@@ -0,0 +1,7 @@
+''' Faça um programa que inicialize e crie uma lista com os valores de 1 até 10 e depois exiba apenas os números pares. '''
+
+for numero in range(1, 11):
+    if numero % 2 == 0:
+        print(numero)
+
+
---
+++ Licao-05-LacosDeRepeticaoparte1/tabuada_multiplicacao.py
@@ -0,0 +1,4 @@
+for operador in range(1, 11):
+    for operador_2 in range(1, 11):
+        resultado = operador * operador_2
+        print(f"{operador} X {operador_2} = {resultado} \n")
